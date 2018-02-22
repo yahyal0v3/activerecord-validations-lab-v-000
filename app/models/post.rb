401bb5dfd9ffdea2c1e39ee551439dd2d2a01f63
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-02-22 16:49:46 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates(:content, { :length => { :minimum => 250 } })
+  validates(:summary, { :length => { :maximum => 250 } })
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  validate :clickbait
+
+  def clickbait
+    if title
+      errors.add(:title, 'not clickbait') unless title.match(/Won't Believe | Secret | Top \d+ | Guess/)
+    end
+  end
 end
