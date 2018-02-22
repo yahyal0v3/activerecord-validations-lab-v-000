0bacc6bb1d134efbfc8e18077cdca3bf6347c0b0
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-02-22 16:37:19 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates(:content, { :length => { :minimum => 250 } })
+  validates(:summary, { :length => { :maximum => 250 } })
+  validates :category, inclusion: { in: %w(fiction non-fiction)}
+  validate :clickbait
+
+  def clickbait
+    if title
+      if !title.include?("Won't Believe") && !title.include?("Secret") && !title.include?("Top") && !title.include?("Guess")
+        errors.add(:title, "non-clickbait")
+      end
+    end
+  end
 end
