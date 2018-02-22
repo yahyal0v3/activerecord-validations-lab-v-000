87cfe27febd93a54079e90123a303a38408820a4
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-02-22 16:40:15 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates(:content, { :length => { :minimum => 250 } })
   validates(:summary, { :length => { :maximum => 250 } })
-  validates :category, inclusion: { in: %w(fiction non-fiction)}
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
   validate :clickbait
 
   def clickbait
